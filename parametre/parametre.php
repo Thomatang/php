e62d16645201587573f24c8b0aebf2f9c3990cfb
Zhang Tsinxias <zhangtsinxias@gmail.com>
2018-02-13 14:01:26 +0100

    parameter url

---
+++ parametre/parametre.php
@@ -0,0 +1,44 @@
+<?php
+// http://localhost:8000/parametre.php?nom=Nemare&prenom=Jean&age=52&dateDebut=2/05/2016&dateFin=27/11/2016&langage=PHP&serveur=LAMP&semaine=12&batiment=12&salle=101
+
+// exo1
+echo "Exo 1<br>";
+echo $_GET["nom"] . "<br>";
+echo $_GET["prenom"] . "<br><br>";
+
+// exo2
+echo "Exo 2<br>";
+echo $_GET["age"] . "<br><br>";
+
+// exo3
+echo "Exo 3<br>";
+echo $_GET["dateDebut"] . "<br>";
+echo $_GET["dateFin"] . "<br><br>";
+
+// exo4
+echo "Exo 4<br>";
+echo $_GET["langage"] . "<br>";
+echo $_GET["serveur"] . "<br><br>";
+
+// exo5
+echo "Exo 5<br>";
+echo $_GET["semaine"] . "<br><br>";
+
+// exo6
+echo "Exo 6<br>";
+echo $_GET["batiment"] . "<br>";
+echo $_GET["salle"] . "<br><br>";
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+ </head>
+ <body>
+
+ </body>
+ </html>
