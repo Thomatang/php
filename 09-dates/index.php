7d703a76b563a5f6c058e3c8656bd5b92d62c803
Zhang Tsinxias <zhangtsinxias@gmail.com>
2018-02-15 12:08:59 +0100

    add calendar builder

--- 09-dates/index.php
+++ 09-dates/index.php
@@ -51,36 +51,92 @@ echo date("d-m-Y", strtotime($date. "- 22 days"));
 
 // TP
 echo "<h2>TP</h2>";
-// $month= date ("m");
-// $year=date("Y");
-// $day=date("d");
-// $endDate=date("t",mktime(0,0,0,$month,$day,$year));
-// echo '<font face="arial" size="2">';
-// echo '<table align="center" border="0" cellpadding=5 cellspacing=5 style=""><tr><td align=center>';
-// echo "Today : ".date("F, d Y ",mktime(0,0,0,$month,$day,$year));
-// echo '</td></tr></table>';
-// echo '<table align="center" border="0" cellpadding=1 cellspacing=1 style="border:1px solid #CCCCCC">
-// <tr bgcolor="#EFEFEF">
-// <td align=center><font color=red>Lundi</font></td>
-// <td align=center>Mardi</td>
-// <td align=center>Mercredi</td>
-// <td align=center>Jeudi</td>
-// <td align=center>Vendredi</td>
-// <td align=center>Samedi</td>
-// <td align=center>Dimanche</td>
-// </tr>';
-// $s=date ("w", mktime (0,0,0,$month,1,$year));
-// for ($ds=1;$ds<=$s;$ds++) {
-// echo "<td style=\"font-family:arial;color:#B3D9FF\" align=center valign=middle bgcolor=\"#FFFFFF\">
-// </td>";}
-// for ($d=1;$d<=$endDate;$d++) {
-// if (date("w",mktime (0,0,0,$month,$d,$year)) == 0) { echo "<tr>"; }
-// $fontColor="#000000";
-// if (date("D",mktime (0,0,0,$month,$d,$year)) == "Sun") { $fontColor="red"; }
-// echo "<td style=\"font-family:arial;color:#333333\" align=center valign=middle> <span style=\"color:$fontColor\">$d</span></td>";
-// if (date("w",mktime (0,0,0,$month,$d,$year)) == 6) { echo "</tr>"; }}
-// echo '</table>';
-// echo "<p style=\"text-align:center;color:#555;\"><a style=\"text-decoration:none;color:#555;\" href='http://tutorial.world.edu/web-development/how-to-create-simple-dynamic-calendar-table-php/'>PHP Calendar Table</a> by <a style=\"text-decoration:none;color:#555;\" href='http://tutorial.world.edu'>Tutorial.World.Edu</a></p>";
+
+function build_calendar($month,$year,$dateArray) {
+     // Create array days of week.
+     $daysOfWeek = array('Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi');
+
+     // What is the first day of the month in question?
+     $firstDayOfMonth = mktime(0,0,0,$month,1,$year);
+
+     // How many days does this month contain?
+     $numberDays = date('t',$firstDayOfMonth);
+
+     // Retrieve some information about the first day of the
+     // month in question.
+     $dateComponents = getdate($firstDayOfMonth);
+
+     // What is the name of the month in question?
+     $monthName = $dateComponents['month'];
+
+     // What is the index value (0-6) of the first day of the
+     // month in question.
+     $dayOfWeek = $dateComponents['wday'];
+
+     // Create the table tag opener and day headers
+     $calendar = "<table class='calendar'>";
+     $calendar .= "<caption>$monthName $year</caption>";
+     $calendar .= "<tr>";
+
+     // Create the calendar headers
+     foreach($daysOfWeek as $day) {
+          $calendar .= "<th class='header'>$day</th>";
+     }
+
+     // Create the rest of the calendar
+     // Initiate the day counter, starting with the 1st.
+     $currentDay = 1;
+     $calendar .= "</tr><tr>";
+
+     // The variable $dayOfWeek is used to
+     // ensure that the calendar
+     // display consists of exactly 7 columns.
+     if ($dayOfWeek > 0) {
+          $calendar .= "<td colspan='$dayOfWeek'>&nbsp;</td>";
+     }
+
+     $month = str_pad($month, 2, "0", STR_PAD_LEFT);
+
+     while ($currentDay <= $numberDays) {
+          // Seventh column (Saturday) reached. Start a new row.
+          if ($dayOfWeek == 7) {
+
+               $dayOfWeek = 0;
+               $calendar .= "</tr><tr>";
+
+          }
+
+          $currentDayRel = str_pad($currentDay, 2, "0", STR_PAD_LEFT);
+          $date = "$year-$month-$currentDayRel";
+          $calendar .= "<td class='day' rel='$date'>$currentDay</td>";
+
+          // Increment counters
+          $currentDay++;
+          $dayOfWeek++;
+
+     }
+
+     // Complete the row of the last week in month, if necessary
+     if ($dayOfWeek != 7) {
+          $remainingDays = 7 - $dayOfWeek;
+          $calendar .= "<td colspan='$remainingDays'>&nbsp;</td>";
+     }
+
+     $calendar .= "</tr>";
+     $calendar .= "</table>";
+     return $calendar;
+
+}
+
+$dateComponents = getdate();
+// $month = $dateComponents['mon']; // today's calendar
+// $year = $dateComponents['year']; // today's calendar
+$month = $_POST['month'];
+$year = $_POST['year'];
+if (isset($_POST['month']) && isset($_POST['year'])) {
+  echo build_calendar($month,$year,$dateArray);
+}
+
  ?>
 
 <!DOCTYPE html>
@@ -92,6 +148,59 @@ echo "<h2>TP</h2>";
   <title>Document</title>
 </head>
 <body>
-
+  <h2>Calendar Build</h2>
+  <form class="" action="index.php" method="POST">
+    <label for="month">Month</label>
+    <select class="" name="month">
+      <option value="1">Janvier</option>
+      <option value="2">Février</option>
+      <option value="3">Mars</option>
+      <option value="4">Avril</option>
+      <option value="5">Mai</option>
+      <option value="6">Juin</option>
+      <option value="7">Juillet</option>
+      <option value="8">Août</option>
+      <option value="9">Septembre</option>
+      <option value="10">Octobre</option>
+      <option value="11">Novembre</option>
+      <option value="12">Décembre</option>
+    </select>
+    <br>
+    <label for="year">Year</label>
+    <select class="" name="year">
+      <option value="1996">1996</option>
+      <option value="1997">1997</option>
+      <option value="1998">1998</option>
+      <option value="1999">1999</option>
+      <option value="2000">2000</option>
+      <option value="2001">2001</option>
+      <option value="2002">2002</option>
+      <option value="2003">2003</option>
+      <option value="2004">2004</option>
+      <option value="2005">2005</option>
+      <option value="2006">2006</option>
+      <option value="2007">2007</option>
+      <option value="2008">2008</option>
+      <option value="2009">2009</option>
+      <option value="2010">2010</option>
+      <option value="2011">2011</option>
+      <option value="2012">2012</option>
+      <option value="2013">2013</option>
+      <option value="2014">2014</option>
+      <option value="2015">2015</option>
+      <option value="2016">2016</option>
+      <option value="2017">2017</option>
+      <option value="2018">2018</option>
+      <option value="2019">2019</option>
+      <option value="2020">2020</option>
+      <option value="2021">2021</option>
+      <option value="2022">2022</option>
+      <option value="2023">2023</option>
+      <option value="2024">2024</option>
+      <option value="2025">2025</option>
+    </select>
+    <br>
+    <button type="submit" name="button">Submit</button>
+  </form>
 </body>
 </html>
